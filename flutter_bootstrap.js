0ffb8e6892b0b93f911e3b80c3a78552d305ca9d
Christian Salazar <christiansalazarh@gmail.com>
2026-06-03 23:06:58 -0400

    repara problema de subida de archivos en target: web (android y linux funcionan bien). causa: en web, el uploader no recibe la ruta del archivo (path) sino que los bytes; se crearon dos casos entonces.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2466112721"
+    serviceWorkerVersion: "59685538"
   }
 });
